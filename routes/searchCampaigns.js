b0339905b90f649226cdea35dc4586ab9088a7f8
Ricky Waldron <ricky_waldron@hotmail.com>
2018-01-17 17:17:22 +0100

    almostDone

--- routes/searchCampaigns.js
+++ routes/searchCampaigns.js
@@ -2,20 +2,18 @@ require('dotenv').load();
 
 module.exports = (app, client) => {
     app.get('/searchCampaigns', (req, res) => {
-        res.render("searchCampaigns")
+        let id = req.session.user_id
+        res.render("searchCampaigns", {id: id})
     })  
 
     app.post('/searchCampaigns', (req, res) => {
             let suggest = req.body.searchedCampaign
-            console.log(suggest)
             const query = {
                 text: (`SELECT * FROM campaigns WHERE title ILIKE '%${suggest}%'`)
             }
             client.query(query, (error, result) => {
                 if (error) throw error
-                    console.log(result.rows[0])
-                    campaign = result.rows[0]
-                    console.log(campaign.title)
+                    campaign = result.rows    
                     res.send(campaign)
                 })
             })
